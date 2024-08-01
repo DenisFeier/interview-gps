3d3b083a84b33c5ed6cd845f4b85759ea26ef915
Denis Feier <denisfeier@Deniss-MacBook-Pro.local>
2024-08-02 00:45:50 +0300

    init

---
+++ index.ts
@@ -0,0 +1,22 @@
+/**
+ * Task: Given a list of coordinates and an origin coordinate, find the first n closest coordinates to the origin based on their Euclidean distance.
+ * Coordinates: A pair of numbers representing a point in a 2D space, e.g., (x, y).
+ * Exemple for [[1, 2], [3, 4], [1, -1], [2, 2], [5, 3]] we should get [[1, -1], [1, 2], [2, 2]] for n = 3 and origin [0, 0]
+ * Also, write a test case.
+ *
+ * distance = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+ *
+ */
+
+type Coordinate = [number, number];
+
+function findClosestCoordinates(
+    coordinates: Coordinate[],
+    origin: Coordinate,
+    n: number
+): Coordinate[] {
+    /**
+     * YOUR CODE HERE
+     */
+    return [];
+}
